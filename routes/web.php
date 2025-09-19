ffe36ab8dcb20df98d3b49b72675e706bddf5d10
bjquyum <a.ahmed@straj.com>
2025-09-19 12:01:47 +0100

    First Debug

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // Profile routes
     // Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 });
